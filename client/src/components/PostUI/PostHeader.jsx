690271efb8ed0d5b9451066a5216e469e0b466c6
Aharon Cohen <ayc@live.co.uk>
2024-12-08 00:28:51 +0200

    done for the night

--- client/src/components/PostUI/PostHeader.jsx
+++ client/src/components/PostUI/PostHeader.jsx
@@ -1,45 +1,12 @@
 import React from "react";
 
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
+
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-// Utility functions
-
-//initials for avatars
-function stringAvatar(name) {
-    if (!name) return { children: "?" };
-    const nameParts = name.split(" ");
-    return {
-        sx: {
-            bgcolor: stringToColor(name),
-        },
-        children: `${nameParts[0][0]}${nameParts[1]?.[0] || ""}`.toUpperCase(),
-    };
-}
-//avatar color
-function stringToColor(string) {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    let color = "#";
-    for (let i = 0; i < 3; i++) {
-        const value = (hash >> (i * 8)) & 0xff;
-        color += `00${value.toString(16)}`.slice(-2);
-    }
-    return color;
-}
+import { Link } from "react-router-dom";
+import { timeAgo } from "../../utils/timeAgo.js";
+import { stringAvatar } from "../../utils/avatarStyler.js";
 
 export default function PostHeader({ post }) {
     return (
@@ -47,13 +14,16 @@ export default function PostHeader({ post }) {
             avatar={
                 <Avatar
                     {...stringAvatar(post.posterId?.username || "Unknown User")}
+                    sx={{
+                        cursor: "pointer",
+                        ...stringAvatar(post.posterId?.username).sx,
+                    }}
+                    component={Link}
+                    to={`/user/${post.posterId?.username}`}
                 />
             }
             title={post.posterId?.username || "Title not found"}
-            subheader={new Date(post.createdAt).toLocaleDateString()}
-            <Box>
-            
-            </Box>
+            subheader={timeAgo(post.createdAt)}
         />
     );
 }
