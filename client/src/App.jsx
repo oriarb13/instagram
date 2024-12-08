efd2b1b3701f0789a30fd2bcd49c296b7c6b86e7
oriarb13 <oriarb13690@gmail.com>
2024-12-08 10:57:14 +0200

    before marge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import CreatePostPage from "./pages/CreatePostPage/CreatePostPage.jsx";
 import Login from "./components/AuthForm/LogIn.jsx"
 import SignUp from "./components/AuthForm/SignUp.jsx";
 import ProfilePage from "./components/Profilepage/Profilepage.jsx";
+import ProfileUserPage from "./components/Profilepage/Profile_user_page.jsx";
 
 //import Components
 import NavBar from "./components/PageLayout/Navbar";
@@ -30,8 +31,8 @@ function App() {
                 <Route path="/create-post" element={<CreatePostPage />} />
                 <Route path="*" element={<NotFound />} />
                 <Route path="/profile" element={<ProfilePage />} />
-
-
+                {/* הוספתי */}
+                <Route path="/userPage/:username" element={<ProfileUserPage />} />
             </Routes>
         </>
     );
